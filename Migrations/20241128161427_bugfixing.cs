8edc7b9a696ea3554b76b113736d64a0752ae554
Awais Qadeer <webtechssolution@gmail.com>
2024-11-28 21:41:03 +0500

    test

---
+++ Migrations/20241128161427_bugfixing.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rems_Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class bugfixing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalArea",
+                table: "Listings",
+                newName: "PropertyType");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Listings",
+                newName: "Phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CurrencyType",
+                table: "Listings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Listings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrencyType",
+                table: "Listings");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Listings");
+
+            migrationBuilder.RenameColumn(
+                name: "PropertyType",
+                table: "Listings",
+                newName: "TotalArea");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Listings",
+                newName: "Description");
+        }
+    }
+}
